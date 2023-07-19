e4b54dc08ebf6cb41e8b41dfce5741c3cac2f350
ise2005best <93008391+ise2005best@users.noreply.github.com>
2023-07-19 16:29:22 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar1 from './components/navBar';
+import HomePage from './components/displayPage';
+function App() {
+  return (
+    <div >
+    <Navbar1/>
+    <HomePage/>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/displayPage.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import Picture from '../images/image-hero-desktop.png'
+import logo1 from '../images/client-audiophile.svg'
+import logo2 from '../images/client-databiz.svg'
+import logo3 from '../images/client-meet.svg'
+import logo4 from '../images/client-maker.svg'
+const HomePage = () => {
+    return (
+        <div>
+            <NavigationMenu.Root>
+                <div>
+                    <img src={Picture} alt="home" className='major-picture' />
+                </div>
+                <p className='major-text'>
+                    Make remote work
+                </p>
+                <div className='container-2'>
+                    <p className='more-text'>
+                        Get your team in sync, no matter your location.
+                        Streamline processes, create team rituals, and watch productivity soar.
+                    </p>
+
+                    <button className='learn-more-button'>
+                        Learn More
+                    </button>
+                   
+                </div>
+                <div className='botton-logos'>
+                    <img src={logo2} alt='databiz' className='databiz-picture' />
+                    <img src={logo1} alt='audio-phile' className='audioPhile-picture' /> 
+                    <img src={logo3} alt='meet' className='meet-picture' />
+                    <img src={logo4} alt='maker' className='maker-picture' />
+                    </div>
+
+                {/* <NavigationMenu.Link className='learn-more-button' href=''>
+            Learn More
+            </NavigationMenu.Link> */}
+
+
+            </NavigationMenu.Root>
+
+
+        </div>
+
+    )
+}
+export default HomePage;
